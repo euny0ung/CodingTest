79c939ceff528b84830bf5ed739354386a0143a7
euny0ung <56199780+euny0ung@users.noreply.github.com>
2023-03-05 13:42:21 +0900

    [Bronze II] Title: Hashing, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.cc
+++ 백준/Bronze/15829. Hashing/Hashing.cc
@@ -2,21 +2,32 @@
 #include <cmath>
 using namespace std;
 
+int pow_func(int x, int cnt) {
+
+	long long out = 1;
+
+	for (int i = 0; i < cnt; i++) {
+		out *= x;
+		/*cout << result << endl;*/
+	}
+	
+	return out;
+}
+
 int main() {
 
 	char ch;
 	int n;
 	int cnt = 0;
-	int result = 0;
+	long long result = 0;
 
 	cin >> n;
 
 	for (int i = 0; i < n; i++) {
 		cin >> ch;
-		result += ((int)ch - 96) * pow(31, cnt);
-		result %= 1234567891;
+		result += ((int)ch - 96) * pow_func(31, cnt)%1234567891;
 		// 모듈러 연산은 정수만 가능한데 pow때문에 double형됨.
-
+	
 		cnt++;
 	}
 
