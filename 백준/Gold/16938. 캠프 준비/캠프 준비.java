c0ea807a04c4f9f96d58f445605d6a803545867b
euny0ung <56199780+euny0ung@users.noreply.github.com>
2023-09-02 00:05:08 +0900

    [Gold V] Title: 캠프 준비, Time: 232 ms, Memory: 24356 KB -BaekjoonHub

--- 백준/Gold/16938. 캠프 준비/캠프 준비.java
+++ 백준/Gold/16938. 캠프 준비/캠프 준비.java
@@ -1,13 +1,9 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
-import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Main {
