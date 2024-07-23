1f69136375bffa5092c66102b0148d885ff73b86
euny0ung <56199780+euny0ung@users.noreply.github.com>
2024-07-23 23:43:00 +0900

    [Gold IV] Title: 부분합, Time: 8 ms, Memory: 2288 KB -BaekjoonHub

--- 백준/Gold/1806. 부분합/부분합.cc
+++ 백준/Gold/1806. 부분합/부분합.cc
@@ -3,8 +3,12 @@
 using namespace std;
 
 int main() {
-    int N=0;
-    long S=0;
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+
+    int N = 0;
+    long S = 0;
     cin >> N >> S;
 
     int arr[100000];
@@ -13,17 +17,15 @@ int main() {
     }
 
     long sum = 0;
-    int minLength = N + 1; // 초기값은 불가능한 큰 값으로 설정
+    int minLength = N + 1;
 
     int start = 0, end = 0;
     while (end < N) {
-        // sum에 arr[end]를 더하고 end를 증가시킨다
         while (sum < S && end < N) {
             sum += arr[end];
             end++;
         }
 
-        // sum이 S 이상이 되면 start를 증가시키며 최소 길이를 찾는다
         while (sum >= S) {
             minLength = min(minLength, end - start);
             sum -= arr[start];
@@ -31,11 +33,11 @@ int main() {
         }
     }
 
-    // minLength가 초기값 그대로이면 부분 배열을 찾을 수 없는 경우이다
     if (minLength == N + 1) {
-        cout << 0 << endl;
-    } else {
-        cout << minLength << endl;
+        cout << 0 << "\n";
+    }
+    else {
+        cout << minLength << "\n";
     }
 
     return 0;
