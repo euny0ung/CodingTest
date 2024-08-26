4197d6902631ca38b9027d5cd1ad045b9da2bedc
euny0ung <56199780+euny0ung@users.noreply.github.com>
2024-08-26 14:07:37 +0900

    [Silver V] Title: 돌 게임, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/9655. 돌 게임/돌 게임.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	int n;
+	cin >> n;
+
+	if (n % 2 == 0) {
+		cout << "CY" << endl;
+	}
+	else {
+		cout << "SK" << endl;
+	}
+
+	return 0;
+}
