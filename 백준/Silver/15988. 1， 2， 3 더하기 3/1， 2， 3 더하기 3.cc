dbb0c927b05d708a768477b7767dc576aadf20eb
euny0ung <56199780+euny0ung@users.noreply.github.com>
2024-08-26 15:01:35 +0900

    [Silver II] Title: 1, 2, 3 더하기 3, Time: 8 ms, Memory: 9836 KB -BaekjoonHub

--- 백준/Silver/15988. 1， 2， 3 더하기 3/1， 2， 3 더하기 3.cc
+++ 백준/Silver/15988. 1， 2， 3 더하기 3/1， 2， 3 더하기 3.cc
@@ -1,32 +1,36 @@
 #include <iostream>
 #include <vector>
+#define MOD 1000000009
 using namespace std;
 
-long long calc(int n, vector<long long> &dp) {
-	for (int i = 3; i <= n; i++) {
-		dp[i] = dp[i - 1] % 1000000009 + dp[i - 2] % 1000000009 + dp[i - 3] % 1000000009;
+// 메모이제이션 최적화
+
+int T = 0, N = 0;
+vector<long long>dp(1000000 + 1, 1);
+
+void init() {
+	dp[1] = 1;
+	dp[2] = 2;
+	dp[3] = 4;
+
+	for (int i = 3; i <= 1000000; i++) {
+		dp[i] = (dp[i - 1] + dp[i - 2] + dp[i - 3])% MOD;
 	}
 
-	return dp[n];
 }
 
 int main() {
 
 	ios_base::sync_with_stdio(false);
-
-	int T = 0, N = 0;
+	cin.tie(0);
 
 	cin >> T;
 
+	init();
+
 	while (T--) {
 		cin >> N;
-
-		vector<long long>dp(N + 1, 1);
-
-		dp[2] = 2;
-
-		if (N < 3) cout << dp[N] << "\n";
-		else cout << calc(N, dp) % 1000000009 << "\n";
+		cout << dp[N] << "\n";
 	}
 
 	return 0;
