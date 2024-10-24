ffc4aef880a3c11ae8e0bc6df536ffe0e109a6ad
euny0ung <56199780+euny0ung@users.noreply.github.com>
2024-10-24 23:52:21 +0900

    [Silver V] Title: 모바일 광고 입찰, Time: 164 ms, Memory: 2804 KB -BaekjoonHub

---
+++ 백준/Silver/31246. 모바일 광고 입찰/모바일 광고 입찰.cc
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+
+	int N = 0, K = 0;
+	int a = 0, b = 0;
+	int cnt = 0;
+
+	vector<int>v;
+
+	cin >> N >> K;
+
+	for (int i = 0; i < N; i++) {
+		cin >> a >> b;
+		if (a >= b) cnt++; // 이미 낙찰
+		else {
+			int diff = b - a;
+			v.push_back(diff);
+		}
+	}
+
+	// 이미 입찰 끝났으면
+	if (cnt >= K) cout << 0 << "\n";
+	else {
+		sort(v.begin(), v.end());
+
+		int answer = v[K - cnt - 1];
+
+		cout << answer << "\n";
+	}
+
+	
+
+	return 0;
+}
