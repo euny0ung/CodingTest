2b3c73fe52f68dde8952d9e9ec8c0cdd965bc1e1
euny0ung <56199780+euny0ung@users.noreply.github.com>
2024-11-18 21:58:01 +0900

    [Silver III] Title: 소수 구하기, Time: 160 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.cc
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.cc
@@ -2,31 +2,26 @@
 using namespace std;
 
 int main() {
+
 	ios::sync_with_stdio(false);
-	cin.tie(NULL);
+	cin.tie(0);
 
-	int n1=0, n2=0;
-	int cnt = 0;
+	int N = 0, M = 0;
 
-	cin >> n1>>n2;
+	cin >> N >> M;
 
-
-	for (int i = n1; i <= n2; i++) {
+	for (int i = N; i <= M; i++) {
+		int cnt = 0;
 
 		if (i == 1) continue;
 
 		for (int j = 1; j*j <= i; j++) {
-				if (i % j == 0) cnt++;
-				if (cnt > 1)break;
-			}
+			if (i % j == 0) cnt++;
+			if (cnt > 1) break;
+		}
+
 		if (cnt == 1) cout << i << "\n";
-		cnt = 0;
 	}
 
 	return 0;
 }
-
-// 1. for문을 사용하여 나누어 떨어지는 경우 count++. count가 2인 경우에만 출력
-// 이렇게 하면 시간초과 뜸..
-// 2. 루트 이용
-// 3. 1은 소수가 아님... 이 부분을 놓쳤음
