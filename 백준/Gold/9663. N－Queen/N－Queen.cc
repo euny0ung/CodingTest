794a3eed79960597aa04c795c9d9063b05381424
euny0ung <56199780+euny0ung@users.noreply.github.com>
2025-02-16 22:32:47 +0900

    [Gold IV] Title: N-Queen, Time: 4664 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.cc
+++ 백준/Gold/9663. N－Queen/N－Queen.cc
@@ -1,53 +1,39 @@
 #include <iostream>
-#include <math.h>
 using namespace std;
 
+int N, ans;
+int map[16];
 
-int N = 0;
-int row[15] = { 0, };
-int cnt = 0;
+void queen(int cnt) {
 
-// x는 열
-void dfs(int x) {
-
-	// 마지막 행까지 탐색한 경우
-	if (x == N) {
-		cnt++;
+	if (cnt == N) {
+		ans++;
 		return;
 	}
 
-
-	// 0행~n행까지 모든 이전 열들을 검사 후 조건에 맞으면 다음 행에 퀸 놓기
 	for (int i = 0; i < N; i++) {
-		bool isSafe = true;
-
-		row[x] = i;
-
-		// 모든 이전 열
-		for (int j = 0; j < x; j++) {
-			if (row[x] == row[j] || abs(x - j) == abs(row[x] - row[j])) {
-				isSafe = false;
+		map[cnt] = i;
+		int check = true;
+		for (int j = 0; j < cnt; j++) {
+			if (map[cnt] == map[j] || cnt == j || (abs(cnt - j) == abs(map[cnt] - map[j]))) {
+				check = false;
 				break;
 			}
 		}
-
-		if (isSafe) {
-			dfs(x + 1);
-		}
+		if (check) queen(cnt + 1);
 	}
-
 }
 
 int main() {
 
-	ios_base::sync_with_stdio(false);
-	cin.tie(0);
-
 	cin >> N;
 
-	dfs(0);
+	for (int i = 0; i < N; i++) {
+		map[0] = i;
+		queen(1);
+	}
 
-	cout << cnt << "\n";
+	cout << ans << "\n";
 
 	return 0;
 }
